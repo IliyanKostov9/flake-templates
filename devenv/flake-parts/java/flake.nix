b061288e0ece9a908b969fa53d12161a603f7263
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-10-30 21:12:33 +0200

    feat: added devenv for flake-parts in java

---
+++ devenv/flake-parts/java/flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Java project";
+
+  inputs = {
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    devenv.url = "github:cachix/devenv";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs@{ flake-parts, devenv-root, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        packages.default = pkgs.jdk8;
+
+        devenv.shells.default = {
+          name = "Java project";
+          # packages = [ config.packages.default ];
+          languages.java.enable = true;
+          languages.java.jdk.package = pkgs.jdk8;
+          languages.java.maven.enable = true;
+
+          devenv.root =
+            let
+              devenvRootFileContent = builtins.readFile devenv-root.outPath;
+            in
+            pkgs.lib.mkIf (devenvRootFileContent != "") devenvRootFileContent;
+        };
+      };
+    };
+}
